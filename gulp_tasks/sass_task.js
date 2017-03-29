f5422ca79bc82e4f434c3d83e27a46c081cb31d5
Jinma Yamashita <jinmayamashita@gmail.com>
2017-03-29 16:41:53 +0900

    update .gitignore

--- gulp_tasks/sass_task.js
+++ gulp_tasks/sass_task.js
@@ -1,11 +1,25 @@
 const config = require('../config.json')
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
 gulp.task('sass', function () {
+	let plugins = [
+	  autoprefixer({
+			browsers: [
+				'last 3 version',
+				'ie >= 9',
+				'Android >= 4',
+				'ios_saf >= 8'
+			]
+		})
+  ]
+
 	return gulp.src(config.PATH.src.styles)
 		.pipe(sass())
 		.on('error', sass.logError)
+		.pipe(postcss(plugins))
 		.pipe(gulp.dest(config.PATH.dist))
 		.pipe(browserSync.stream())
 })
