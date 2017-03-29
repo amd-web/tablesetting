38fcffe0eb85d522d9daed1b0b67c21c78063cb6
Jinma Yamashita <jinmayamashita@gmail.com>
2017-03-29 12:16:43 +0900

    import browsersync for browser testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
+// require
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create();
+
 // config file
 const config = require('./config.json')
 
-// require
-const gulp = require('gulp')
 
 // tasks
-gulp.task('default', [], function () {
+gulp.task('html', function () {
 	return gulp.src(config.PATH.src.html)
 		.pipe(gulp.dest(config.PATH.dist))
+		.pipe(browserSync.stream())
 })
+
+// Static server
+gulp.task('server', function() {
+	
+	const staticServer = config.PATH.dist
+
+  browserSync.init({
+  	https: false,
+		open: false,
+		ghostMode: false,
+		logLevel: 'debug',
+    server: { baseDir: staticServer }
+  })
+})
+
+gulp.task('default', ['html', 'server'])
