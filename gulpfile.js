d38fee1c6e35434600185772501855df079f47c2
Jinma Yamashita <jinmayamashita@gmail.com>
2017-03-29 11:52:46 +0900

    set dev path

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+// config file
+const config = require('./config.json')
+
 // require
 const gulp = require('gulp')
 
 // tasks
 gulp.task('default', [], function () {
-  console.log('works')
+	return gulp.src(config.PATH.src.html)
+		.pipe(gulp.dest(config.PATH.dist))
 })
