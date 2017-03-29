36fcd7d7e1b0749cb9f53ae8133a1f4d52e4b065
Jinma Yamashita <jinmayamashita@gmail.com>
2017-03-29 16:27:48 +0900

    added html-injector

--- gulp_tasks/html_task.js
+++ gulp_tasks/html_task.js
@@ -4,5 +4,5 @@ const gulp = require('gulp')
 gulp.task('html', function () {
 	return gulp.src(config.PATH.src.html)
 		.pipe(gulp.dest(config.PATH.dist))
-		.pipe(browserSync.stream())
+		// .pipe(browserSync.stream())
 })
--- gulp_tasks/server_task.js
+++ gulp_tasks/server_task.js
@@ -3,12 +3,22 @@ const gulp = require('gulp')
 
 gulp.task('server', function() {
 	const staticServer = config.PATH.dist
+
   browserSync.init({
   	https: false,
 		open: false,
 		ghostMode: false,
 		logLevel: 'debug',
-    server: { baseDir: staticServer }
+    server: { baseDir: staticServer },
+    plugins: [
+      {
+        module: 'bs-html-injector',
+        options: {
+          files: ['dist/index.html']
+        }
+      }
+    ]
   })
   gulp.watch(config.PATH.src.styles, ['sass'])
+  gulp.watch(config.PATH.src.html, ['html'])
 })
